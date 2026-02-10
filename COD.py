b7468797a61e2c6848ed9c2f066a4125c97aa67d
ArtemPasechnik007 <artempasechnik007@gmail.com>
2026-02-10 18:40:30 +0300

    v 6.0

--- COD.py
+++ COD.py
@@ -2,15 +2,26 @@
 temp = int(input("Введите температуру (°C): "))
 rain = int(input("Есть осадки? (1/2) 1 - да 2 - нет: "))
 
+
 if rain == 1 and 30 > temp > 20 :
-    print("Наденьте Футболку, Шорты, дождевик")
+    print("Наденьте Футболку, Шорты и возьмите Дождевик")
 
-elif rain == 2 and 30 > temp > 20 :
+if rain == 2 and 30 > temp > 20 :
     print("Наденьте Футболку, Шорты")
+if 20 > temp > 0  :
+    level_rain = int(input("Осадки сильные? (1/2) 1 - да 2 - нет: "))
 
-elif temp < 0:
-    print("Наденьте пуховик")
+    if 20 > temp > 0 and rain == 1 and level_rain == 1:
+        print("Наденьте Пальто, Резиновые сапоги и возьмите Зонт")
 
-elif temp > 0 and rain == 2 :
-    print("Наденьте пальто")
+    if 20 > temp > 0 and rain == 1 and level_rain == 2:
+        print("Наденьте Пальто и возьмите Дождевик")
+
+if temp < 0:
+    print("Наденьте Пуховик")
+
+if 20 > temp > 0  and rain == 2 :
+    print("Наденьте Пальто")
+
+
 
